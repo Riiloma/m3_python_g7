0216ee3612e4542aa478c1a52cea20b2ebcd2dfc
Livio <livio.gutierrez.v@gmail.com>
2024-03-14 11:39:57 -0300

    documentando codigo de archivo escape

--- dia3_desafio1/escape.py
+++ dia3_desafio1/escape.py
@@ -1,13 +1,18 @@
 import math
 
+#Datos de prueba
 # gravedad = 9.8
 # radio= 6371
 
+#Pidiendo datos
 gravedad = float(input("Ingrese el la gravedad del planeta: "))
 radio = int(input("Ingrese el radio en Kilómetros: "))
 
+#Pasndo radio de kilometros a metros
 radio_metros = radio * 1000
 
+#Formula para saber la velocidad de escape
 resultado = (math.sqrt(2*gravedad*radio_metros))
 
+#Mostrandole al usuario la velocidad de escape al usuario
 print(f"La velocidad de Escape es: {round(resultado,1)} [m/s]")
