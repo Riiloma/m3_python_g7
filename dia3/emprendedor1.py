8f6e7ccd96c0373484fbbf626edc743f931967ca
Livio <livio.gutierrez.v@gmail.com>
2024-03-14 09:20:02 -0300

    añadiendo mensaje

--- dia3/emprendedor1.py
+++ dia3/emprendedor1.py
@@ -1,3 +1,4 @@
+print("ADVERTENCIA: si pone una letra en los valores dara ERROR")
 Precio_sus = float(input("Ingrese el precio de las suscripciones: "))
 Numero_usuario = int(input("Ingrese le numero de Usuario: "))
 Gastos_totales = float(input("Ingrese sus gastos totales: "))
