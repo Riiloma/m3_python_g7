b9a2143d74701e070862bfd0a5cf053a4337f063
Livio <livio.gutierrez.v@gmail.com>
2024-04-01 09:17:43 -0300

    complementando con mas comentarios el codigo

--- dia13/desafio/filtro.py
+++ dia13/desafio/filtro.py
@@ -1,4 +1,13 @@
+""" 
+pruebas:
+    python filtro.py 30000
+    python filtro.py 30000 menor
+    python filtro.py 30000 mayor
+    python filtro.py 30000 otro
+"""
+
 import sys
+#Diccionario
 precios = {'Notebook': 700000,
 'Teclado': 25000,
 'Mouse': 12000,
@@ -6,6 +15,7 @@ precios = {'Notebook': 700000,
 'Escritorio': 135000,
 'Tarjeta de Video': 1500000}
 
+#Funcion para la operacion del mayor que 
 def filtrar_mayor(precios, umbral):
     filtro_mayor = {}
     for key, valor in precios.items():
@@ -13,6 +23,7 @@ def filtrar_mayor(precios, umbral):
             filtro_mayor[key] = valor
     return filtro_mayor
 
+#Funcion para la operacion del menor que 
 def filtrar_menor(precios, umbral):
     filtro_menor = {}
     for key, valor in precios.items():
@@ -22,14 +33,17 @@ def filtrar_menor(precios, umbral):
 
 print(sys.argv)
 
+#Invocando a la funcion filtar_mayor solo con el argumento en el indice 1
 if len(sys.argv) == 2:
     umbral= int(sys.argv[1])
     almacenando= filtrar_mayor(precios, umbral)
     print("Los productos mayores al umbral son: " + " ".join(almacenando))
     
-elif len(sys.argv) == 3:
-    umbral= sys.argv[2]
+elif len(sys.argv) == 3:#detectando el largo de sys.argv
+    umbral= sys.argv[1]
     criterio= sys.argv[2]
+    
+    #Invocando a la funcion filtar_mayor solo con el argumento en el indice 2 y 3
     if criterio == "mayor":
         umbral= int(sys.argv[1])
         almacenando= filtrar_mayor(precios, umbral)
